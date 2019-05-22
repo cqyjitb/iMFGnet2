15372a20e29210d1c1fa28e3c825e50060d053d8
idmurder <17924529@qq.com>
2019-05-22 11:43:26 +0800

    1: prd 917110140 2019-05-21

---
+++ core/src/main/java/com/hand/hap/comm/DataSourceExchange.java
@@ -0,0 +1,32 @@
+package com.hand.hap.comm;
+
+import org.aspectj.lang.JoinPoint;
+
+public class DataSourceExchange {
+
+    public void before(JoinPoint point) {
+
+        //获取目标对象的类类型
+        Class<?> aClass = point.getTarget().getClass();
+        String c = aClass.getName();
+        String[] ss = c.split("\\.");
+        //获取包名用于区分不同数据源
+        String packageName = ss[2];
+
+        if ("kanb".equals(packageName)) {
+            DataSourceHolder.setDataSources(DataSourceEnum.mySqlDataSource.getKey());
+            System.out.println("数据源："+DataSourceEnum.mySqlDataSource.getKey());
+        } else {
+            DataSourceHolder.setDataSources(DataSourceEnum.mainDataSource.getKey());
+            System.out.println("数据源："+DataSourceEnum.mainDataSource.getKey());
+        }
+    }
+
+    /**
+     * 执行后将数据源置为空
+     */
+    public void after() {
+        DataSourceHolder.setDataSources(null);
+    }
+
+}
