f955520325c46757715c8e921ffe9aa19b95a84e
phx1056628025 <1056628025@qq.com>
2019-05-28 11:50:06 +0800

    1、新增看板设备维护及看板设备维护对话框页面 2019.5.28 918100064

--- core/src/main/java/yj/kanb/equipment/controllers/EquimentContorller.java
+++ core/src/main/java/yj/kanb/equipment/controllers/EquimentContorller.java
@@ -8,6 +8,7 @@ import com.hand.hap.system.controllers.BaseController;
 import com.hand.hap.system.dto.ResponseData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -56,5 +57,50 @@ public class EquimentContorller  extends BaseController {
         List<Equipment> list =  service.queryEquipment(dto);
         return new ResponseData(list);
     }
-
+    /**
+     * 看板设备维护页面删除请求 918100064
+     * @param request
+     * @param dto
+     * @return
+     */
+    @RequestMapping(value = {"/equipment/removeEquipment"})
+    @ResponseBody
+    public ResponseData removeEquipment(HttpServletRequest request,@RequestBody List<Equipment> dto){
+        service.deleteEquipment(dto);
+        return new ResponseData();
+    }
+    /**
+     * 看板设备维护页面添加请求 918100064
+     * @param request
+     * @param dto
+     * @return
+     */
+    @RequestMapping(value = {"/equipment/insertEquipment"})
+    @ResponseBody
+    public ResponseData insertEquipment(HttpServletRequest request,@RequestBody List<Equipment> dto){
+        IRequest requestContext = createRequestContext(request);
+        ResponseData rs = new ResponseData();
+        String userId = "" + request.getSession().getAttribute("userId");
+        String result = service.insertEquipment(dto,userId);
+        if(result != null){
+            rs.setSuccess(false);
+            rs.setMessage(result);
+            return rs;
+        }
+        return new ResponseData();
+    }
+    /**
+     * 看板设备维护页面修改请求 918100064
+     * @param request
+     * @param dto
+     * @return
+     */
+    @RequestMapping(value = {"/equipment/updateEquipment"})
+    @ResponseBody
+    public ResponseData updateEquipment(HttpServletRequest request,@RequestBody List<Equipment> dto){
+        IRequest requestContext = createRequestContext(request);
+        String userId = "" + request.getSession().getAttribute("userId");
+        service.updateEquipment(dto,userId);
+        return new ResponseData();
+    }
 }
--- core/src/main/java/yj/kanb/equipment/service/impl/EquipmentServiceImpl.java
+++ core/src/main/java/yj/kanb/equipment/service/impl/EquipmentServiceImpl.java
@@ -8,7 +8,9 @@ import yj.kanb.equipment.dto.Equipment;
 import yj.kanb.equipment.mapper.EquipmentMapper;
 import yj.kanb.equipment.service.IEquipmentService;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -23,5 +25,42 @@ public class EquipmentServiceImpl extends BaseServiceImpl<Equipment> implements 
     @Override
     public List<Equipment> queryEquipment(Equipment dto) {
         return equipmentMapper.selectEquipment(dto);
+    }
+
+    @Override
+    public void deleteEquipment(List<Equipment> dto) {
+        if(dto.size() > 0){
+            for(int i=0;i<dto.size();i++){
+                equipmentMapper.deleteEquipment(dto.get(i));
+            }
+        }
+    }
+
+    @Override
+    public String insertEquipment(List<Equipment> dto, String userId) {
+        if(dto.size() > 0){
+            Equipment result = equipmentMapper.selectByMac(dto.get(0).getMac());
+            if(result == null){
+                Equipment equipment = dto.get(0);
+                String eqId = UUID.randomUUID().toString();
+                equipment.setEqId(eqId);
+                equipment.setCreatedBy(Long.valueOf(userId));
+                equipment.setCreationDate(new Date());
+                equipmentMapper.insertEquipment(equipment);
+            }else{
+                return "该MAC地址已存在，请重新输入！";
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void updateEquipment(List<Equipment> dto, String userId) {
+        if(dto.size() > 0){
+            Equipment equipment = dto.get(0);
+            equipment.setLastUpdatedBy(Long.valueOf(userId));
+            equipment.setLastUpdateDate(new Date());
+            equipmentMapper.updateEquipment(equipment);
+        }
     }
 }
