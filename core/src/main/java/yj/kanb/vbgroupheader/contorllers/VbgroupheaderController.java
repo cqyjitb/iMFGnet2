78c66ac269aaf69cb66a36e7e8fee3fcd78de262
phx1056628025 <1056628025@qq.com>
2019-05-31 09:29:31 +0800

    1、看板设备维护修改 2、新增看板车间产线组头维护页面 2019.5.31 918100064

--- core/src/main/java/yj/kanb/vbgroupheader/contorllers/VbgroupheaderController.java
+++ core/src/main/java/yj/kanb/vbgroupheader/contorllers/VbgroupheaderController.java
@@ -1,9 +1,26 @@
 package yj.kanb.vbgroupheader.contorllers;
 
 import com.hand.hap.system.controllers.BaseController;
+import com.hand.hap.system.dto.ResponseData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import yj.kanb.vbgroupheader.dto.Vbgroupheader;
+import yj.kanb.vbgroupheader.service.IVbgroupheaderService;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class VbgroupheaderController extends BaseController {
+    @Autowired
+    private IVbgroupheaderService service;
+
+    @RequestMapping(value = {"/groupH/queryGroupH"})
+    @ResponseBody
+    public ResponseData queryGroupH(HttpServletRequest request, Vbgroupheader dto){
+
+        return new ResponseData();
+    }
 }
--- core/src/main/java/yj/kanb/vblinegroupheader/mapper/VblinegroupheaderMapper.java
+++ core/src/main/java/yj/kanb/vblinegroupheader/mapper/VblinegroupheaderMapper.java
@@ -3,5 +3,31 @@ package yj.kanb.vblinegroupheader.mapper;
 import com.hand.hap.mybatis.common.Mapper;
 import yj.kanb.vblinegroupheader.dto.Vblinegroupheader;
 
+import java.util.List;
+
 public interface VblinegroupheaderMapper extends Mapper<Vblinegroupheader> {
+    /**
+     * 看板车间产线组头表查询 918100064
+     * @param dto
+     * @return
+     */
+    List<Vblinegroupheader> selectLineGroupH(Vblinegroupheader dto);
+
+    /**
+     * 看板车间产线组头表删除 918100064
+     * @param dto
+     */
+    void deleteLineGroupH(Vblinegroupheader dto);
+
+    /**
+     * 看板车间产线组头表修改 918100064
+     * @param dto
+     */
+    void updateLineGroupH(Vblinegroupheader dto);
+
+    /**
+     * 看板车间产线组头表添加 918100064
+     * @param dto
+     */
+    void insertLineGroupH(Vblinegroupheader dto);
 }
