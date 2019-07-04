4f9fbf39ad5e66ae74611577a81d7a8d4f938042
idmurder <17924529@qq.com>
2019-07-04 16:46:14 +0800

    1:升级 接口管理 917110140

---
+++ core/src/main/java/com/hand/hap/message/websocket/DefaultWebSocketHandler.java
@@ -0,0 +1,59 @@
+package com.hand.hap.message.websocket;
+
+
+import com.hand.hap.system.service.IBadgeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.*;
+
+/**
+ * 
+ * @author hailin.xu@hand-china.com
+ * @author njq.niu@hand-china.com
+ *
+ */
+public class DefaultWebSocketHandler implements WebSocketHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(DefaultWebSocketHandler.class);
+    
+
+
+    @Autowired
+    private WebSocketSessionManager webSocketSessionManager;
+
+    @Autowired
+    private IBadgeService badgeService;
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        webSocketSessionManager.addSession(session);
+        badgeService.initBadgeMessage(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        String userName = session.getPrincipal().getName();
+        if(session.isOpen()){
+            session.close();
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        webSocketSessionManager.removeSession(session);
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+
+
+
+
+}
