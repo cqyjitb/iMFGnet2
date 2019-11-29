3f3b331d314b310eb1e2103b9563dbc3b795072a
phx1056628025 <1056628025@qq.com>
2019-11-29 16:49:46 +0800

    压射号及报工统计表(财务) 2019.11.29 918100064

--- core/src/main/java/yj/core/wipshotinput/controllers/ShotInputController.java
+++ core/src/main/java/yj/core/wipshotinput/controllers/ShotInputController.java
@@ -9,13 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import yj.core.wipshotinput.dto.ShotInput;
 import yj.core.wipshotinput.service.IShotInputService;
-import yj.core.wipshotnum.dto.Shotnum;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -38,61 +36,30 @@ public class ShotInputController extends BaseController {
         dto.setPrdDateBefore(dto.getPrdDateBefore().substring(0,10));
         List<ShotInput> list = service.selectShotInput(dto);
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar cal2 = Calendar.getInstance();
         if (list.size() > 0){
-            List<ShotInput> list2 = new ArrayList<ShotInput>();
             for (int i=0;i<list.size();i++){
-                if (!("3".equals(list.get(i).getShifts()))){
-                    list2.add(list.get(i));
+                if ((list.get(i).getYeild()==0)&&(list.get(i).getWasteNum()==0)){
+                    list.remove(i);
+                    i--;
                 }
             }
             for (int i=0;i<list.size();i++){
                 ShotInput shotInput = list.get(i);
-                if (shotInput.getShifts().equals("1")){
+                if (i == 0){
                     shotInput.setCheckError(0L);
-                }else {
+                }else{
+                    Date prdDateBefore = null;
+                    Date prdDateAfter = null;
                     try {
-                        cal2.setTime(sf.parse(shotInput.getPrdDate()));
+                        prdDateBefore = sf.parse(list.get(i-1).getPrdDate());
+                        prdDateAfter = sf.parse(shotInput.getPrdDate());
                     } catch (ParseException e) {
                         e.printStackTrace();
                     }
-                    Integer week = cal2.get(Calendar.WEEK_OF_YEAR) % 2;
-                    if ((cal2.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) && (("0".equals(shotInput.getShiftSeq()))
-                            || (("1".equals(shotInput.getShiftSeq())) && (week == 1)) || (("2".equals(shotInput.getShiftSeq())) && (week == 0)))) {
-                        if (shotInput.getShifts().equals("2")) {
-                            for (int j=0;j<list2.size();j++){
-                                if ((shotInput.getArbpl().equals(list2.get(j).getArbpl()))&&(shotInput.getPrdDate().equals(list2.get(j).getPrdDate()))&&(list2.get(j).getShifts().equals("1"))){
-                                    shotInput.setCheckError(list2.get(j).getShotEnd()-shotInput.getShotStart());
-                                    list2.remove(j);
-                                    break;
-                                }
-                            }
-                        }else{
-                            for (int j=0;j<list2.size();j++){
-                                if ((shotInput.getArbpl().equals(list2.get(j).getArbpl()))&&(shotInput.getPrdDate().equals(list2.get(j).getPrdDate()))&&(list2.get(j).getShifts().equals("2"))){
-                                    shotInput.setCheckError(list2.get(j).getShotEnd()-shotInput.getShotStart());
-                                    list2.remove(j);
-                                    break;
-                                }
-                            }
-                            if (shotInput.getCheckError() == null){
-                                for (int j=0;j<list2.size();j++){
-                                    if ((shotInput.getArbpl().equals(list2.get(j).getArbpl()))&&(shotInput.getPrdDate().equals(list2.get(j).getPrdDate()))&&(list2.get(j).getShifts().equals("1"))){
-                                        shotInput.setCheckError(list2.get(j).getShotEnd()-shotInput.getShotStart());
-                                        list2.remove(j);
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }else{
-                        for (int j=0;j<list2.size();j++){
-                            if ((shotInput.getArbpl().equals(list2.get(j).getArbpl()))&&(shotInput.getPrdDate().equals(list2.get(j).getPrdDate()))&&(list2.get(j).getShifts().equals("1"))){
-                                shotInput.setCheckError(list2.get(j).getShotEnd()-shotInput.getShotStart());
-                                list2.remove(j);
-                                break;
-                            }
-                        }
+                    if (prdDateAfter.before(prdDateBefore)){
+                        shotInput.setCheckError(0L);
+                    }else {
+                        shotInput.setCheckError(shotInput.getShotStart()-list.get(i-1).getShotEnd());
                     }
                 }
             }
